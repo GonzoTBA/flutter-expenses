844cb3da877ff504ffbed12465a1903a2f84cf51
Javier Malonda <jmalonda@gmail.com>
2023-08-18 12:18:28 +0200

    Implemented Balance screen

--- lib/screens/balance_screen.dart
+++ lib/screens/balance_screen.dart
@@ -11,79 +11,118 @@ class BalanceScreen extends StatefulWidget {
 
 class BalanceScreenState extends State<BalanceScreen> {
   final DatabaseReference _database = FirebaseDatabase.instance.ref();
+  List<String> userIDs = [];
   int currentUserTotal = 0;
   int otherUserTotal = 0;
+  String otherUserName = ''; // Nombre del otro usuario
+  int expenseDifference = 0; // Diferencia de gastos entre los usuarios
+  String currentUserName = '';
+  int expenseDifferenceFinal = 0;
+  String higherPayerName = '';
 
   @override
   void initState() {
     super.initState();
-    _calculateExpenses();
-    _listenForAuthChanges();
-  }
-
-  void _listenForAuthChanges() {
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user != null) {
-        // Usuario autenticado, calcula los gastos
-        _calculateExpenses();
-      } else {
-        // Usuario no autenticado, reinicia los totales
-        setState(() {
-          currentUserTotal = 0;
-          otherUserTotal = 0;
-        });
-      }
+    _readUserIDs().then((ids) {
+      setState(() {
+        userIDs = ids;
+      });
+      _calculateBalances().then((_) {
+        _prepareData();
+      });
     });
+    print('User Ids: $userIDs');
   }
 
-  Future<void> _calculateExpenses() async {
-    final currentUser = FirebaseAuth.instance.currentUser;
-    if (currentUser == null) {
-      return; // No hay usuario autenticado
-      print('No user authenticated');
+  void _prepareData() {
+    // Calculate difference
+    expenseDifferenceFinal = expenseDifference.abs();
+    // Get higher payer name
+    if (expenseDifference >= 0) {
+      higherPayerName = currentUserName;
+    } else {
+      higherPayerName = otherUserName;
     }
-
-    final currentUserID = currentUser.uid;
-    final dataSnapshot = await _database.child('expenses').child(currentUserID).once();
-    final currentUserExpenses = dataSnapshot.snapshot.value as Map<dynamic, dynamic>?;
-
-    print('Current user expenses: $currentUserExpenses');
-
-    int currentUserTotal = 0;
-    if (currentUserExpenses != null) {
-      currentUserExpenses.forEach((expenseID, expenseData) {
-        final amount = expenseData['amount'] as int;
-        currentUserTotal += amount;
-      });
-    }
-
-    print('Current user total: $currentUserTotal');
-
-    // Obtén la lista de usuarios autenticados
-    final usersSnapshot = await _database.child('users').once();
-    final usersData = usersSnapshot.snapshot.value as Map<dynamic, dynamic>?;
-
-    int otherUserTotal = 0;
-    if (usersData != null) {
-      usersData.forEach((userID, userData) {
-        // Excluye el usuario actual
-        if (userID != currentUserID) {
-          final userExpenses = userData['expenses'] as Map<dynamic, dynamic>?;
-          if (userExpenses != null) {
-            userExpenses.forEach((expenseID, expenseData) {
-              final amount = expenseData['amount'] as int;
-              otherUserTotal += amount;
-            });
-          }
-        }
-      });
-    }
-
     setState(() {
-      this.currentUserTotal = currentUserTotal;
-      this.otherUserTotal = otherUserTotal;
+      expenseDifferenceFinal = expenseDifferenceFinal;
+      higherPayerName = higherPayerName;
     });
   }
+
+  Future<void> _calculateBalances() async {
+    print('Executing function _calculateBalances');
+    if (userIDs.isNotEmpty) {
+      final currentUser = FirebaseAuth.instance.currentUser;
+
+      if (currentUser != null) {
+        final String currentUserEmail = currentUser.email as String;
+        final currentUserId = currentUser.uid;
+        final currentUserTotal = await calculateTotalExpensesForUser(currentUserId);
+        
+        // Get current User Name
+        if (currentUserEmail.contains('jmalonda')) {
+          currentUserName = 'Javier';
+          otherUserName = 'Daniela';
+        } else {
+          currentUserName = 'Daniela';
+          otherUserName = 'Javier';
+        }
+        print('Current user name: $currentUserName');
+        setState(() {
+          this.currentUserTotal = currentUserTotal;
+        });
+
+        if (userIDs.length >= 2) {
+          final otherUserID = userIDs.firstWhere((id) => id != currentUserId);
+
+          final otherUserTotal = await calculateTotalExpensesForUser(otherUserID);
+          setState(() {
+            this.otherUserTotal = otherUserTotal;
+            expenseDifference = currentUserTotal - otherUserTotal;
+          });
+        }
+      }
+    }
+  }
+
+  Future<List<String>> _readUserIDs() async {
+    final usersSnapshot = await _database.child('expenses').once();
+    print('Printing usersSnapshot: $usersSnapshot');
+
+    if (usersSnapshot.snapshot.value != null) {
+      final Map<dynamic, dynamic>? usersData = usersSnapshot.snapshot.value as Map<dynamic, dynamic>?;
+      print('Printing usersData: $usersData');
+
+      if (usersData != null) {
+        userIDs = usersData.keys.cast<String>().toList();
+        print('Printing userIDs: $userIDs');
+
+        return userIDs;
+      }
+    }
+
+    return [];
+  }
+
+  Future<int> calculateTotalExpensesForUser(String userID) async {
+  final userExpensesSnapshot = await _database.child('expenses').child(userID).once();
+
+  int totalExpenses = 0;
+
+  if (userExpensesSnapshot.snapshot.value != null) {
+    final Map<dynamic, dynamic>? userExpensesData = userExpensesSnapshot.snapshot.value as Map<dynamic, dynamic>?;
+    
+    if (userExpensesData != null) {
+      userExpensesData.forEach((expenseID, expenseData) {
+        final amount = expenseData['amount'] as int;
+        totalExpenses += amount;
+      });
+    }
+  }
+
+  return totalExpenses;
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -91,22 +130,25 @@ class BalanceScreenState extends State<BalanceScreen> {
       appBar: AppBar(
         title: const Text('Balance'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Your total expenses: $currentUserTotal euros'),
-            Text('Other user\'s total expenses: $otherUserTotal euros'),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('$currentUserName total expenses: $currentUserTotal euros',
+                style: const TextStyle(fontSize: 20.0)),
+              Text('$otherUserName total: $otherUserTotal euros',
+                style: const TextStyle(fontSize: 20.0)),
+              const SizedBox(height: 20.0),
+              Text('$higherPayerName has a positive balance of $expenseDifferenceFinal euros',
+                style: const TextStyle(fontSize: 25.0)),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-void main() {
-  runApp(const MaterialApp(
-    home: BalanceScreen(),
-  ));
-}
