5104cea3dfb79101d798441b8336b8ed27f48a27
Javier Malonda <jmalonda@gmail.com>
2024-08-07 17:03:14 +0200

    Added strategy to prevent multiple expense entries when expense cannot be instantly saved

--- lib/screens/add_expense_screen.dart
+++ lib/screens/add_expense_screen.dart
@@ -16,6 +16,7 @@ class AddExpenseScreenState extends State<AddExpenseScreen> {
   final TextEditingController _descriptionController = TextEditingController();
   final FocusNode _amountFocus = FocusNode();
   final DatabaseReference _database = FirebaseDatabase.instance.ref();
+  bool _isSubmitting = false;
 
   @override
   void initState() {
@@ -27,46 +28,76 @@ class AddExpenseScreenState extends State<AddExpenseScreen> {
   }
 
   Future<void> _submitExpense(int amount, String description) async {
-    if (description == '') {
+    if (description.isEmpty) {
       description = '---';
     }
-    if (amount > 0) {
+    if (amount > 0 && !_isSubmitting) {
+      setState(() {
+        _isSubmitting = true;
+      });
+
       final user = FirebaseAuth.instance.currentUser;
       final userID = user?.uid;
 
       if (userID != null) {
         final newExpenseRef = _database.child('expenses').child(userID).push();
         final expense = Expense(
-          id: newExpenseRef.key, 
-          amount: amount, 
-          description: description, 
-          timestamp: DateTime.now()
+          id: newExpenseRef.key,
+          amount: amount,
+          description: description,
+          timestamp: DateTime.now(),
         );
-        await newExpenseRef.set(expense.toJson());
+        try {
+          await newExpenseRef.set(expense.toJson());
 
+          Fluttertoast.showToast(
+            msg: 'Expense saved successfully!',
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.TOP,
+            timeInSecForIosWeb: 3,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            fontSize: 16.0,
+          );
 
+          _amountController.clear();
+          _descriptionController.clear();
+
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            _amountFocus.requestFocus();
+          });
+        } catch (e) {
+          Fluttertoast.showToast(
+            msg: 'Failed to save expense. Please try again.',
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.TOP,
+            timeInSecForIosWeb: 3,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0,
+          );
+        } finally {
+          setState(() {
+            _isSubmitting = false;
+          });
+        }
+      } else {
+        setState(() {
+          _isSubmitting = false;
+        });
         Fluttertoast.showToast(
-          msg: 'Expense saved successfully!',
+          msg: 'User not authenticated. Please log in.',
           toastLength: Toast.LENGTH_LONG,
           gravity: ToastGravity.TOP,
           timeInSecForIosWeb: 3,
-          backgroundColor: Colors.green,
+          backgroundColor: Colors.red,
           textColor: Colors.white,
           fontSize: 16.0,
         );
       }
-
-      _amountController.clear();
-      _descriptionController.clear();
-
-      _amountFocus.unfocus();
-
-    } else {
-      ScaffoldMessenger.of(_scaffoldContext!).showSnackBar(
-        const SnackBar(content: Text('Please enter a valid amount.')),
-      );
     }
   }
+
 
   BuildContext? _scaffoldContext; // Variable para capturar el contexto del Scaffold
 
@@ -100,13 +131,13 @@ class AddExpenseScreenState extends State<AddExpenseScreen> {
             ),
             const SizedBox(height: 16.0),
             ElevatedButton(
-              onPressed: () {
-                final int amount = int.tryParse(_amountController.text) ?? 0;
-                final String description = _descriptionController.text;
-                _submitExpense(amount, description);
-              },
-              child: const Text('Submit Expense'),
-            ),
+            onPressed: _isSubmitting ? null : () {
+              final int amount = int.tryParse(_amountController.text) ?? 0;
+              final String description = _descriptionController.text;
+              _submitExpense(amount, description);
+            },
+            child: _isSubmitting ? const CircularProgressIndicator() : const Text('Submit Expense'),
+            )
 
             // Logout button for development purposes
             // ElevatedButton(
